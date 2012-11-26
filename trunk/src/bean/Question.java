e2736b41e8aed2747c9296fcc8c2f1eb6e5ca317
stephane.sikora@gmail.com <stephane.sikora@gmail.com@6494685f-3dff-d182-588e-4d6a1340f4f5>
2012-11-26 10:56:59 +0000

    Bean Parcours Bean Question Bean Reponse Bean Resultat

--- trunk/src/bean/Question.java
+++ trunk/src/bean/Question.java
@@ -9,4 +9,38 @@ package bean;
  */
 public class Question {
 
+		private long id;
+		private String intitule;
+		/**
+		 * 
+		 */
+		public Question() {
+			super();
+		}
+		/**
+		 * @return the id
+		 */
+		public long getId() {
+			return id;
+		}
+		/**
+		 * @param id the id to set
+		 */
+		public void setId(long id) {
+			this.id = id;
+		}
+		/**
+		 * @return the intitule
+		 */
+		public String getIntitule() {
+			return intitule;
+		}
+		/**
+		 * @param intitule the intitule to set
+		 */
+		public void setIntitule(String intitule) {
+			this.intitule = intitule;
+		}
+		
+		
 }
--- trunk/src/bean/Resultat.java
+++ trunk/src/bean/Resultat.java
@@ -9,4 +9,27 @@ package bean;
  */
 public class Resultat {
 
+		private long id;
+
+		/**
+		 * 
+		 */
+		public Resultat() {
+			super();
+		}
+
+		/**
+		 * @return the id
+		 */
+		public long getId() {
+			return id;
+		}
+
+		/**
+		 * @param id the id to set
+		 */
+		public void setId(long id) {
+			this.id = id;
+		}
+		
 }
